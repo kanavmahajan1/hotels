671edcd12476ba69083cf2d87b72e93aec0b3835
Kanav Mahajan <mahajankanavkth@gmail.com>
2026-01-29 23:17:31 +0530

    Add JWT authentication and protected routes

--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); 
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -14,8 +15,28 @@ const personSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
+  password: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+  
+  }
   // Add more fields as needed
 });
  // Just for learning
+personSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// ✅ compare password
+personSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('Person', personSchema);
--- server.js
+++ server.js
@@ -3,6 +3,41 @@ const app = express();
 const db = require("./db"); 
 const Person = require("./models/Person");
 app.use(express.json());
+ const passport= require('passport');
+ const LocalStrategy = require('passport-local').Strategy;
+ const {jwtAuthMiddleware, generateToken} = require('./jwt');
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+    try {
+        // console.log('Received credentials:', username, password);
+        const user = await Person.findOne({ username });
+        if (!user)
+         return done(null, false, { message: 'Incorrect username.' });
+        
+        const isPasswordMatch = await user.comparePassword(password);
+        if (isPasswordMatch)
+            return done(null, user);
+        else
+            return done(null, false, { message: 'Incorrect password.' })
+    } catch (error) {
+        return done(error);
+    }
+}));
+ app.use(passport.initialize());
+
+
+ // Middleware to log requests
+
+  const requestLogger = (req, res, next) => {
+  const time = new Date().toLocaleString();
+
+  const url = req.originalUrl;
+
+  console.log(`[${time}]  ${url}`);
+
+  next(); // very important
+};
+app.use(requestLogger);
 
 app.get("/", (req, res) => {
   res.send("Hello from Express");
@@ -10,15 +45,99 @@ app.get("/", (req, res) => {
 app.get("/s", (req, res) => {
   res.send("Hello from Chandigarh" );
 });
-app.post("/persons", async (req, res) => {
+app.get("/persons", async (req, res) => {
   try {
-    const person = new Person(req.body);  
-    await person.save();
-    res.status(201).send(person);
-  } catch (error) {
-    res.status(400).send(error);
+    const persons = await Person.find();
+    res.status(200).json(persons);
+  } catch (error) { 
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch persons" });
   }
 });
+
+app.post('/signup', async (req, res) =>{
+    try{
+        const data = req.body // Assuming the request body contains the person data
+
+        // Create a new Person document using the Mongoose model
+        const newPerson = new Person(data);
+
+        // Save the new person to the database
+        const response = await newPerson.save();
+        console.log('data saved');
+
+        const payload = {
+            id: response.id,
+            username: response.username
+        }
+        console.log(JSON.stringify(payload));
+        const token = generateToken(payload);
+        console.log("Token is : ", token);
+
+        res.status(200).json({response: response, token: token});
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
+// Login Route
+  app.post('/login', async(req, res) => {
+    try{
+        // Extract username and password from request body
+        const {username, password} = req.body;
+
+        // Find the user by username
+        const user = await Person.findOne({username: username});
+
+        // If user does not exist or password does not match, return error
+        if( !user || !(await user.comparePassword(password))){
+            return res.status(401).json({error: 'Invalid username or password'});
+        }
+
+        // generate Token 
+        const payload = {
+            id: user.id,
+            username: user.username
+        }
+        const token = generateToken(payload);
+
+        // resturn token as response
+        res.json({token})
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Profile route
+ app.get('/profile', jwtAuthMiddleware, async (req, res) => {
+    try{
+        const userData = req.user;
+        console.log("User Data: ", userData);
+
+        const userId = userData.id;
+        const user = await Person.findById(userId);
+
+        res.status(200).json({user});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+// GET method to get the person
+app.get('/', jwtAuthMiddleware, async (req, res) =>{
+    try{
+        const data = await Person.find();
+        console.log('data fetched');
+        res.status(200).json(data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
 
 app.listen(3000, () => {
   console.log(`Server running on port 3000`);
